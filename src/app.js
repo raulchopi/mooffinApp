954ff3ace735b58736fd373fb8e237a54904765b
Raúl Cacabelos Bote <raul.cacabelos@gmail.com>
2016-01-17 18:36:33 +0100

    Se pasa las funciones de inicio al nuevo fichero 'initializer'

--- src/app.js
+++ src/app.js
@@ -2,26 +2,13 @@
 
 import 'driftyco/ionic-bower';
 
-
 import APIService from './general/services/APIService';
-
 import zadiaNG from '../zadia-ng/zadia-ng';
-
 import appConfig from './appConfig';
+import initializer from './initializer';
 
 angular.module('mooffin', ['ionic', zadiaNG])
 .service('APIService', APIService)
 .config(appConfig)
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-});
+.run(initializer);
---
+++ src/initializer.js
@@ -0,0 +1,32 @@
+// import uuid from 'uuid-js';
+
+function initializer($ionicPlatform) {
+  $ionicPlatform.ready(function () {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if (window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+
+    // if($window.cordova && $window.analytics) {
+    //   let trackerId = 'UA-13073417-5';
+    //   // let userId = $localStorage.userId ? $localStorage.userId : uuid.create();
+    //   let userId = uuid.create();
+    //   $localStorage.userId = userId;
+    //
+    //   $cordovaGoogleAnalytics.debugMode();
+    //   $cordovaGoogleAnalytics.startTrackerWithId(trackerId);
+    //   $cordovaGoogleAnalytics.setUserId(userId);
+    //
+    //   console.log(`Analytics started with tracker id ${trackerId}`);
+    // }
+
+  });
+}
+
+initializer.$inject = ['$ionicPlatform', '$cordovaGoogleAnalytics', '$window'];
+
+export default initializer;
